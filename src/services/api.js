94ea4fc3e592f4ad21165bba2b0b115f28800d6c
ViktorPratama <114640656+ViktorPratama@users.noreply.github.com>
2024-12-12 14:32:38 +0900

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-v2-beta.vercel.app/api',
+  baseURL: 'https://backend-v2-beta.vercel.app',
   headers: {
     'Content-Type': 'application/json'
   }
